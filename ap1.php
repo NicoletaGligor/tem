ea53625ea3c479e96c9476efdce5fd7828e70c6d
NicoletaGligor <131368346+NicoletaGligor@users.noreply.github.com>
2023-05-10 14:26:59 +0300

    Add files via upload

---
+++ ap1.php
@@ -0,0 +1,35 @@
+<?php
+$x = 10;
+$y = 5;
+$adunare = $x + $y;
+echo "$x + $y = $adunare";
+?>
+
+<br>
+
+<?php
+$x = 10;
+$y = 5;
+$scadere = $x - $y;
+echo "$x - $y = $scadere";
+?>
+
+<br>
+
+<?php
+$x = 10;
+$y = 5;
+$inmultire = $x * $y;
+echo "$x * $y = $inmultire";
+?>
+
+<br>
+
+<?php
+$x = 10;
+$y = 5;
+$impartire = $x / $y;
+echo "$x / $y = $impartire";
+?>
+
+
